1860fbcb2b0bb4e994aa639b9dff4dd113df0b56
vikierdos <csicsorka@gmail.com>
2024-09-17 11:51:33 +0200

    rossz controller

--- controller/GaleriaController.js
+++ controller/GaleriaController.js
@@ -1,3 +1,50 @@
+import Jatekter from "./view/Jatekter.js";
+import { kepLista } from "./model/adatok.js";
+import NagyKartya from "./view/NagyKartya.js";
+import Model from "./model/model.js";
+
 export default class GaleriaController{
-    
+
+    constructor(){
+    this.jatekterElem = $(".jatekter");
+    this.kivElem = $(".nagykep");
+    this.model = new Model(kepLista,0);
+    this.index = model.getIndex();
+    new NagyKartya(kepLista[index], kivElem);
+    new Jatekter(kepLista, jatekterElem);
+
+    this.kivEsemenykezelo();
+    this.balEsemenyKezelo();
+    this.jobbEsemenyKezelo();
+    }
+
+
+    kivEsemenykezelo(){
+        $(window).on("kivalaszt", (event) => {
+          console.log(event.detail);
+          kivElem.empty();
+          model.setIndex(event.detail.index);
+          new NagyKartya(event.detail, kivElem);
+        });
+
+    }
+
+    jobbEsemenyKezelo(){
+        $(window).on("jobb", (event) => {
+            console.log(event.detail);
+            let index = model.leptetJobb();
+            kivElem.empty();
+          new NagyKartya(kepLista[index], kivElem);
+        });
+
+    }
+    balEsemenyKezelo(){
+        $(window).on("bal", (event) => {
+            console.log(event.detail);
+            let index = model.leptetBal();
+            kivElem.empty();
+          new NagyKartya(kepLista[index], kivElem);
+        });
+
+    }
 }
--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-import Jatekter from "./view/Jatekter.js";
-import { kepLista } from "./model/adatok.js";
-import NagyKartya from "./view/NagyKartya.js";
-import Model from "./model/model.js";
+import GaleriaController from "./controller/GaleriaController.js";
 
-const jatekterElem = $(".jatekter");
-const kivElem = $(".nagykep");
-
-
-const model = new Model(kepLista,0);
-let index = model.getIndex();
-new NagyKartya(kepLista[index], kivElem);
-new Jatekter(kepLista, jatekterElem);
-
-$(window).on("kivalaszt", (event) => {
-  console.log(event.detail);
-  kivElem.empty();
-  model.setIndex(event.detail.index);
-  new NagyKartya(event.detail, kivElem);
-});
-$(window).on("jobb", (event) => {
-    console.log(event.detail);
-    let index = model.leptetJobb();
-    kivElem.empty();
-  new NagyKartya(kepLista[index], kivElem);
-});
-$(window).on("bal", (event) => {
-    console.log(event.detail);
-    let index = model.leptetBal();
-    kivElem.empty();
-  new NagyKartya(kepLista[index], kivElem);
-});
+const controller = new GaleriaController();
